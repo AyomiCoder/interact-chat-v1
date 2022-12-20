88cd45b7574a3f79754ddab1ddbf8ea4f5cb8e9e
AyomiCoder <alukoayomide23@gmail.com>
2022-12-20 17:09:22 +0100

    created profile models

--- interact/models.py
+++ interact/models.py
@@ -3,4 +3,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    user= models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    pic = models.ImageField(upload_to="img", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Friend(models.Model):
+    pass
